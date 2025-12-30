d578d24e6f647f6d86564c686612f8337c87bb57
Beygs <boris.gilles@hotmail.fr>
2025-12-30 16:47:48 +0100

    🐛 fix: fix admin url

--- src/routes/admin/+page.ts
+++ src/routes/admin/+page.ts
@@ -2,5 +2,5 @@ import { PUBLIC_APIURL } from "$env/static/public";
 import { redirect } from "@sveltejs/kit";
 
 export async function load() {
-	return redirect(308, `${PUBLIC_APIURL}/admin`);
+	return redirect(308, `${PUBLIC_APIURL}/admin/content/projects`);
 }
